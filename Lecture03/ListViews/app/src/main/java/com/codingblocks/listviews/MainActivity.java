0373de0ccf6bfde681d775759a01b57d60ef609e
Arnav Gupta <arnav@codingblocks.com>
2017-06-11 13:04:13 +0530

    using custom layout
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture03/ListViews/app/src/main/java/com/codingblocks/listviews/MainActivity.java
+++ Lecture03/ListViews/app/src/main/java/com/codingblocks/listviews/MainActivity.java
@@ -2,16 +2,23 @@ package com.codingblocks.listviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] cbCourses = {
-            "Pandora", "Elixir", "Crux", "Launchpad"
-    };
+//    String[] cbCourses = {
+//            "Pandora", "Elixir", "Crux", "Launchpad"
+//    };
+    ArrayList<Course> cbCourses = generateCourses();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +26,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView courseListView = (ListView) findViewById(R.id.lv_courses);
-        ArrayAdapter<String> courseAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                cbCourses
-        );
+
+//        ArrayAdapter<String> courseAdapter = new ArrayAdapter<String>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                android.R.id.text1,
+//                cbCourses
+//        );
+
+//        courseListView.setAdapter(courseAdapter);
+
+        CourseAdapter courseAdapter = new CourseAdapter();
         courseListView.setAdapter(courseAdapter);
+
     }
 
     static ArrayList<Course> generateCourses () {
@@ -40,5 +53,44 @@ public class MainActivity extends AppCompatActivity {
         courses.add(new Course("Algo++", "Prateek", 1, "Pitampura"));
 
         return courses;
+    }
+
+    class CourseAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return cbCourses.size();
+        }
+
+        @Override
+        public Course getItem(int i) {
+            return cbCourses.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            LayoutInflater li = getLayoutInflater();
+
+            Course thisCourse = getItem(i);
+
+            View itemView = li.inflate(R.layout.list_item_course, null);
+
+            ((TextView)itemView.findViewById(R.id.tv_courseName))
+                    .setText(thisCourse.getName());
+            ((TextView)itemView.findViewById(R.id.tv_centerName))
+                    .setText(thisCourse.getCenter());
+            ((TextView)itemView.findViewById(R.id.tv_courseBatches))
+                    .setText(String.valueOf(thisCourse.getBatches()));
+            ((TextView)itemView.findViewById(R.id.tv_teacherName))
+                    .setText(String.valueOf(thisCourse.getTeacher()));
+
+
+            return itemView;
+        }
     }
 }
