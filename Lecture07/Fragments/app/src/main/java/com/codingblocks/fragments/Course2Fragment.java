1447ecaba7f2cf712f4c6ff7ac60ab71fd4c4261
Arnav Gupta <arnav@codingblocks.com>
2017-06-20 13:50:21 +0530

    add students to course
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture07/Fragments/app/src/main/java/com/codingblocks/fragments/Course2Fragment.java
+++ Lecture07/Fragments/app/src/main/java/com/codingblocks/fragments/Course2Fragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -18,26 +19,23 @@ public class Course2Fragment extends Fragment {
     public static final String ARG_NAME = "name";
     public static final String ARG_TEACHER = "teacher";
     public static final String ARG_LANGUAGE = "language";
+    public static final String ARG_CID = "courseId";
 
 
     private String courseName = "Sample Course";
     private String courseTeacher = "Sample Teacher";
     private String courseLanguage = "Sample Language";
+    private int courseId = 0;
 
     TextView tvName, tvTeacher, tvLang;
+    Button btnAddStudent;
 
     public Course2Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     *
-     * @param name
-     * @param teacher
-     * @param language
-     * @return
-     */
-    public static Course2Fragment newInstance(Course c) {
+
+    public static Course2Fragment newInstance(Course c, int cId) {
 
         Course2Fragment fragment = new Course2Fragment();
 
@@ -45,6 +43,7 @@ public class Course2Fragment extends Fragment {
         args.putString(ARG_LANGUAGE, c.getLanguage());
         args.putString(ARG_NAME, c.getName());
         args.putString(ARG_TEACHER, c.getTeacher());
+        args.putInt(ARG_CID, cId);
 
         fragment.setArguments(args);
         return fragment;
@@ -57,6 +56,7 @@ public class Course2Fragment extends Fragment {
             courseName = getArguments().getString(ARG_NAME);
             courseLanguage = getArguments().getString(ARG_LANGUAGE);
             courseTeacher = getArguments().getString(ARG_TEACHER);
+            courseId = getArguments().getInt(ARG_CID);
         }
     }
 
@@ -68,10 +68,19 @@ public class Course2Fragment extends Fragment {
         tvName = (TextView) rootView.findViewById(R.id.tvCourseName);
         tvTeacher = (TextView) rootView.findViewById(R.id.tvCourseTeacher);
         tvLang = (TextView) rootView.findViewById(R.id.tvCourseLanguage);
+        btnAddStudent = (Button) rootView.findViewById(R.id.btnAddStudent);
 
         tvName.setText(courseName);
         tvTeacher.setText(courseTeacher);
         tvLang.setText(courseLanguage);
+
+        btnAddStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // increment admissions[courseId] which is in MainActivity
+                ((MainActivity) getActivity()).addStudent(courseId);
+            }
+        });
 
         return rootView;
     }
--- Lecture07/Fragments/app/src/main/java/com/codingblocks/fragments/MainActivity.java
+++ Lecture07/Fragments/app/src/main/java/com/codingblocks/fragments/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1, btn2, btn3;
+    TextView tvSc0, tvSc1, tvSc2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,14 @@ public class MainActivity extends AppCompatActivity {
         courses.add(new Course("Launchpad", "Prateek", "C++"));
         courses.add(new Course("Crux", "Sumeet", "Java"));
 
+
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);
+
+        tvSc0 = (TextView) findViewById(R.id.tvStuCount0);
+        tvSc1 = (TextView) findViewById(R.id.tvStuCount1);
+        tvSc2 = (TextView) findViewById(R.id.tvStuCount2);
 
         final FragmentManager fragMan = getSupportFragmentManager();
 
@@ -31,15 +38,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Course course = null;
+                int cid = 0;
 
                 switch (view.getId()) {
-                    case R.id.btn1: course = courses.get(0); break;
-                    case R.id.btn2: course = courses.get(1); break;
-                    case R.id.btn3: course = courses.get(2); break;
+                    case R.id.btn1: course = courses.get(0); cid = 0; break;
+                    case R.id.btn2: course = courses.get(1); cid = 1; break;
+                    case R.id.btn3: course = courses.get(2); cid = 2; break;
                 }
 
                 fragMan.beginTransaction()
-                        .replace(R.id.fragContainer, Course2Fragment.newInstance(course))
+                        .replace(R.id.fragContainer,
+                                Course2Fragment.newInstance(course, cid))
                         .commit();
             }
         };
@@ -48,5 +57,26 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(courseButtonListener);
         btn3.setOnClickListener(courseButtonListener);
 
+    }
+
+    void addStudent(int cid) {
+        switch (cid) {
+            case 0:
+                tvSc0.setText(String.valueOf(
+                        Integer.valueOf(tvSc0.getText().toString()) + 1
+                ));
+
+                break;
+            case 1:
+                tvSc1.setText(String.valueOf(
+                        Integer.valueOf(tvSc1.getText().toString()) + 1
+                ));
+                break;
+            case 2:
+                tvSc2.setText(String.valueOf(
+                        Integer.valueOf(tvSc2.getText().toString()) + 1
+                ));
+                break;
+        }
     }
 }
