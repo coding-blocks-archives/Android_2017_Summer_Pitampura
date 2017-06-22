b40fd0cda590ab2d1b1e0eaf66059c5b15cf1d25
Arnav Gupta <arnav@codingblocks.com>
2017-06-22 10:52:52 +0530

    lec08: fragments
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture08/MoreFragments/app/src/main/java/com/codingblocks/morefragments/MainActivity.java
@@ -0,0 +1,31 @@
+package com.codingblocks.morefragments;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ((Button) findViewById(R.id.btnTabAct))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(
+                                new Intent(MainActivity.this, TabbedActivity.class)
+                        );
+                    }
+                });
+    }
+}
