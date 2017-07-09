741d2f5899144ff36929220874c56d6b5b29e4f8
Arnav Gupta <arnav@codingblocks.com>
2017-07-09 11:42:49 +0530

    alarmmanager basics
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture14/AlarmManager/app/src/main/java/com/codingblocks/alarmmanager/MainActivity.java
@@ -0,0 +1,34 @@
+package com.codingblocks.alarmmanager;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("A", "aaa");
+        PendingIntent pi = PendingIntent.getActivity(
+                this,
+                112,
+                i,
+                PendingIntent.FLAG_ONE_SHOT
+        );
+        am.set(
+                AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + (1000 * 60),
+                pi
+        );
+
+    }
+}
